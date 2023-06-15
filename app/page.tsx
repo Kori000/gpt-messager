04048f83a3926984a6fc27f843d2a2cd765b7787
Kori <kexin@korix.top>
2023-06-15 14:18:27 +0800

    ui: init ui page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className='text-white flex flex-col justify-center items-center h-screen px-2 '>
+      <h1 className='text-5xl font-bold mb-20 '>ChatGPT</h1>
+    </div>
+  );
+};
+
+export default HomePage;
