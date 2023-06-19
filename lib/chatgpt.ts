8f151717d2998eca756aa3e33bd4faff4d5c074c
Kori <kexin@korix.top>
2023-06-19 17:12:41 +0800

    fix: use usable gpt keys

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,6 +1,5 @@
 import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({
-  organization: 'org-ntJd8bSLmpR22o3BLsMLxkLQ',
   apiKey: process.env.OPENAI_API_KEY
 })
 
