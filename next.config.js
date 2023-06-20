c45ceb1f23ab0fd753fa3129c280de8b2246190b
Kori <kexin@korix.top>
2023-06-20 14:56:46 +0800

    fix: prod env

--- next.config.js
+++ next.config.js
@@ -13,6 +13,8 @@ module.exports = {
   ...nextConfig,
   env: {
     GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET
   }
 }
